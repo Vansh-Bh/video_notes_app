f8bc11d8470919d883b6c37a17c1f3e511f4eacf
Vansh-Bh <vanshbhardwaj28134@gmail.com>
2024-10-29 17:04:43 +0530

    feat: can change theme

--- lib/modules/home/home_view.dart
+++ lib/modules/home/home_view.dart
@@ -1,19 +1,31 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:video_notes_app/modules/home/home_controller.dart';
+import 'package:video_notes_app/theme_changer.dart';
 import '../../../widgets/video_grid.dart';
 
 class HomeView extends GetView<HomeController> {
   @override
   Widget build(BuildContext context) {
+    final themeService = Get.find<ThemeService>();
     return Scaffold(
       appBar: AppBar(
         title: Text('My Videos'),
         actions: [
           IconButton(
             icon: Icon(Icons.add),
+            tooltip: 'Add Video',
             onPressed: controller.addVideo,
           ),
+          Obx(() => IconButton(
+                icon: Icon(themeService.isDarkMode.value
+                    ? Icons.light_mode
+                    : Icons.dark_mode),
+                tooltip: themeService.isDarkMode.value
+                    ? 'Switch to Light Mode'
+                    : 'Switch to Dark Mode',
+                onPressed: themeService.switchTheme,
+              )),
         ],
       ),
       body: Obx(() {
